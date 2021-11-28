4b730440d77b83a09b2d0120a93409146a70058a
ivanbielozertsev <ivanbelozercev13@gmail.com>
2021-11-28 15:02:53 +0200

    Home Work 18

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,52 @@
-function mainFunction(callback) {
-    let num1 = +prompt("Введите число 1")
-    let num2 = +prompt('Введите число 2')
-    return callback(num1, num2)
-}
+// function mainFunction(callback) {
+//     let num1 = +prompt("Введите число 1")
+//     let num2 = +prompt('Введите число 2')
+//     return callback(num1, num2)
+// }
  
-function exponentiation(arg1, arg2) {
-    let exp = Math.pow(arg1, arg2);
-    alert(exp)
- }
+// function exponentiation(arg1, arg2) {
+//     let exp = Math.pow(arg1, arg2);
+//     alert(exp)
+//  }
 
-mainFunction(exponentiation)
+// mainFunction(exponentiation)
+
+
+// function mainFunction(callback) {
+//     let num1 = +prompt("Введите число 1")
+//     let num2 = +prompt('Введите число 2')
+//     return callback(num1, num2)
+// }
+ 
+// function multiplay(arg1, arg2) {
+//     let exp = arg1 * arg2;
+//     alert(exp)
+//  }
+
+// mainFunction(multiplay)
+
+// function mainFunction(callback) {
+//     let num1 = +prompt("Введите число 1")
+//     let num2 = +prompt('Введите число 2')
+//     return callback(num1, num2)
+// }
+ 
+// function division(arg1, arg2) {
+//     let exp = arg1 / arg2;
+//     alert(exp)
+//  }
+
+// mainFunction(division)
+
+// function mainFunction(callback) {
+//     let num1 = +prompt("Введите число 1")
+//     let num2 = +prompt('Введите число 2')
+//     return callback(num1, num2)
+// }
+ 
+// function modulo(arg1, arg2) {
+//     let exp = arg1 % arg2;
+//     alert(exp)
+//  }
+
+// mainFunction(modulo)
